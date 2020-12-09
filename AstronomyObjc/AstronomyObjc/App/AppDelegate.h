ce14ebf015567ca701886485be742e4fc8c21d7d
Kenneth Jones <kennethjones1991@outlook.com>
2020-12-08 20:19:56 -0700

    Created and organized initial project

---
+++ AstronomyObjc/AstronomyObjc/App/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AstronomyObjc
+//
+//  Created by Kenneth Jones on 12/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AstronomyObjc/AstronomyObjc/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AstronomyObjc
+//
+//  Created by Kenneth Jones on 12/8/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
